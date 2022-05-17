c5385de629c97276699e586a9f64ef6fbf4d5be9
Shaforostov <dragon090986@gmail.com>
2022-05-17 13:43:17 +0600

    Update

--- src/main/java/com/Aise/Server/repositories/TokenRepository.java
+++ src/main/java/com/Aise/Server/repositories/TokenRepository.java
@@ -2,6 +2,9 @@ package com.Aise.Server.repositories;
 
 import java.sql.Date;
 
+import javax.persistence.Transient;
+import javax.transaction.Transactional;
+
 import com.Aise.Server.models.Token;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +13,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface TokenRepository extends JpaRepository<Token, String> {  
   Token getByToken(String token);
   Boolean existsByToken(String token);
-  void deleteByExpiryDateLessThan(Date date);
+  @Transactional void deleteByExpiryDateLessThan(Date date);
 }
